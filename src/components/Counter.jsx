fddf6a3927b913cbd6719592a7ae6eaa07686f18
Angel Resendiz <angel.resendiz@gmail.com>
2020-12-30 03:27:27 -0600

    [ADD] Inserting Nubesk logo in top bar

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "./../assets/logo-light.png";
 
 const Counter = ({ burneds }) => {
   return (
-    <div className="counter bg-dark text-light">
-      Burned questions: {burneds}
+    <div className="counter bg-dark text-light d-flex justify-content-between">
+      <div>
+        <img alt="logo" src={logo} />
+      </div>
+      <div>Burned questions: {burneds}</div>
     </div>
   );
 };
